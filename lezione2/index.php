7e39ac4a52433d12a81a0af52b471214c442a70a
Andrea Barghigiani <a.barghigiani@gmail.com>
2016-05-09 13:47:10 +0200

    Completata Lezione 2

--- lezione2/index.php
+++ lezione2/index.php
@@ -1,3 +1,53 @@
+//1. Controlliamo il funzionamento del PHP
+<?php
+  phpinfo();
+?>
+
+//2. Apro e chiudo PHP
+<?php
+
+  //Inserisco codice PHP
+
+?>
+
+//3. Apri e chiudi PHP molteplici volte
+<?php
+
+  //Prima porzione codice PHP
+
+?>
+
+<?php
+
+  //Seconda porzione codice PHP
+
+?>
+
+//4. Mancata interpretazione PHP
+<?php
+
+  //Blocco PHP vuoto
+
+?>
+
+phpinfo();
+
+//5. Pagina HTML generata in PHP
+<?php
+  echo "<html>";
+
+  echo "<head>";
+  echo "<title>La mia prima pagina PHP</title>";
+  echo "</head>";
+
+  echo "<body>";
+  echo "<p>Tutto il codice HTML &egrave; stato inserito con il PHP.</p>";
+  echo "</body>";
+
+  echo "</html>";
+?>
+
+//6. Mescoliamo il codice HTML e quello PHP
 <!DOCTYPE html>
 <html>
   <head>
@@ -10,3 +60,20 @@
     </p>
   </body>
 </html>
+
+//7. Apici, virgolette e stringhe
+<?php
+  //Corrette
+  echo "Ciao sono una stringa";
+  echo 'Anche io sono una stringa';
+  echo "Qui vengono usate le \"virgolette\" con un escape";
+  echo 'Mentre qui l\'escape serve per il singolo apice';
+  echo "Volendo pero posso 'mischiare' virgolette e apici";
+  echo 'E anche "fare" il contrario';
+
+  //Sbagliate
+  echo "Pero se inizio con le virgolette';
+  echo 'non posso finire con un singolo apice";
+  echo "Devo anche stare "attento";
+  echo a come equilibrare il tutto";
+?>

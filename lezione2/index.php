4e8327d5934138975834d1f383ed267f24bb446b
Andrea Barghigiani <a.barghigiani@gmail.com>
2016-05-09 11:02:42 +0200

    Codice Lezione 2

---
+++ lezione2/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title><?php echo "La mia prima pagina PHP"; ?></title>
+  </head>
+  <body>
+    <p>
+      <?php echo "Tutto il codice HTML &egrave; stato inserito con il PHP."; ?>
+    </p>
+  </body>
+</html>
